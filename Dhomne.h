2038215cfeec47d830e783fce86e2e9f5f8c5363
purvaj-world <93307792+purvaj-world@users.noreply.github.com>
2021-10-31 14:41:03 +0530

    Creating by Dhomne.h
    This file print the letter in decreasing order

---
+++ Dhomne.h
@@ -0,0 +1,11 @@
+#Code by Purvaj Dhomne 
+W=input('Please enter a string')
+  defense most_frequent(string):
+  d=dict()
+    for key in string:
+if key not in d:
+d[key]=1
+  else:
+d[key]+=1
+  return d
+  print(most_frequebt(W)
